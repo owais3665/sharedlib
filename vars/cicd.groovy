a9da94527a255621254353b9b41fc1d169379c9c
owais3665 <133983497+owais3665@users.noreply.github.com>
2023-06-07 21:30:42 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -13,12 +13,6 @@ def contiDeployment(jobname,ip,context)
   sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
 }
 
-def contiTesting(repo)
-{
-   git "https://github.com/intelliqittrainings/${repo}"
-  
-}
-
 def seliniumTest(jobname)
 {
   sh 'java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar' 
